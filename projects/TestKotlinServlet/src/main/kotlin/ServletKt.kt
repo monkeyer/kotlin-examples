1553e7091dda893e2de295a2b59fed2ab8dc286d
monkeyer <15950194@qq.com>
2017-12-29 18:17:45 +0800

    添加KotlinServlet示例
    成功创建并运行KotlinServlet程序。

---
+++ projects/TestKotlinServlet/src/main/kotlin/ServletKt.kt
@@ -0,0 +1,11 @@
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@WebServlet(name = "Hello", value = ["/", "/hello"])
+class ServletKt: HttpServlet() {
+    override fun doGet(req: HttpServletRequest?, resp: HttpServletResponse?) {
+        resp!!.writer.write("hello, kotlin.")
+    }
+}
