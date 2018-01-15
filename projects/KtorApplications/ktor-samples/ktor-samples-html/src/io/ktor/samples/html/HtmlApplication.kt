93aadca6195a5249b8ffce6739403633eacd9b3e
monkeyer <15950194@qq.com>
2018-01-15 16:15:44 +0800

    添加其它示例项目
    添加其它示例项目

---
+++ projects/KtorApplications/ktor-samples/ktor-samples-html/src/io/ktor/samples/html/HtmlApplication.kt
@@ -0,0 +1,33 @@
+package io.ktor.samples.html
+
+import kotlinx.html.*
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.html.*
+import io.ktor.http.HttpStatusCode
+//import io.ktor.pipeline.*
+import io.ktor.routing.*
+
+fun Application.main() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing) {
+        get("/") {
+            call.respondHtml {
+                head {
+                    title { +"HTML Application" }
+                }
+                body {
+                    h1 { +"Sample application with HTML builders" }
+                    widget {
+                        +"Widgets are just functions"
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun FlowContent.widget(body: FlowContent.() -> Unit) {
+    div { body() }
+}
